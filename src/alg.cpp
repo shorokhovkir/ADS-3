fdc6050c7e8f575956c4082c4ea3a3140682c7b0
shorokhovkir <160240169+shorokhovkir@users.noreply.github.com>
2024-03-24 20:16:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,27 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int cnt = 0, left = 0, right = size - 1;
+  while (right >= left) {
+    int mid = left + (right - left) / 2;
+    if (arr[mid] == value) {
+      cnt++;
+      int ind = mid - 1;
+      while (ind >= 0 && arr[ind] == value) {
+        cnt++;
+        ind--;
+      }
+      ind = mid + 1;
+      while (ind < size && arr[ind] == value) {
+        cnt++;
+        ind++;
+      }
+      return cnt;
+    }
+    else if (arr[mid] < value)
+      left = mid + 1;
+    else
+      right = mid - 1;
+  }
+  return 0;
 }
